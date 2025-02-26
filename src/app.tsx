ce23d7a6b4fe2919b40e3eb5c9e16214828f9fdf
Elliot <hack00mind@gmail.com>
2025-02-26 21:29:55 +0800

    fix: async load script

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,16 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+import { createScriptLoader } from "@solid-primitives/script-loader"
 import "./app.css";
 
 export default function App() {
+  createScriptLoader({
+    src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4587152222007322",
+    async: true,
+    crossorigin: "anonymous",
+  })
+
   return (
     <Router
       root={props => (
